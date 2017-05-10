9287b853b4f1274f5432f006bbefd7fc91eb0c77
Marco <Marco@MarcoPC.fritz.box>
2017-05-10 20:00:44 +0200

    added method "htmlstatement"

--- Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/Customer.java
+++ Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/Customer.java
@@ -36,6 +36,23 @@ class Customer {
 		return result;
 	}
 
+	public String htmlstatement() {
+		Enumeration enum_rentals = rentals.elements();
+		String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+
+		while (enum_rentals.hasMoreElements()) {
+			Rental each = (Rental) enum_rentals.nextElement();
+
+			// show figures for this rental
+			result += each.getMovie().getTitle() + ": " + String.valueOf(each.getCharge()) + "<BR>\n";
+		}
+		// add footer lines
+		result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+		result += "On this rental you earned <EM>" + String.valueOf(getTotalFrequentRenterPoints())
+				+ "</EM> frequent renter points<P>";
+		return result;
+	}
+	
 	private double getTotalCharge() {
 
 		double result = 0;
@@ -48,6 +65,8 @@ class Customer {
 		return result;
 
 	}
+	
+	
 
 	private int getTotalFrequentRenterPoints() {
 		int result = 0;
