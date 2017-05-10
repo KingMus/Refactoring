ebf52b23a8ef8963d086bea0c20ab3f93e2fb890
Marco <Marco@MarcoPC.fritz.box>
2017-05-10 20:51:09 +0200

    used polymorphism on "getCharge" method

--- Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/price/NewReleasePrice.java
+++ Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/price/NewReleasePrice.java
@@ -2,10 +2,16 @@ package price;
 
 import program.Movie;
 
-public class NewReleasePrice extends Price{
+public class NewReleasePrice extends Price {
 
 	public int getPriceCode() {
 		return Movie.NEW_RELEASE;
 	}
+
+	public double getCharge(int daysRented) {
+		return daysRented * 3;
+	}
+	
+	
 
 }
--- Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/price/Price.java
+++ Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/price/Price.java
@@ -1,4 +1,11 @@
 package price;
+
+import program.Movie;
+
 public abstract class Price {
-public abstract int getPriceCode();
+
+	public abstract int getPriceCode();
+
+	public abstract double getCharge(int daysRented);
+
 }
